984a73515adf8b36b7e178ec87f3d60f88d42f6f
xcdh127 <70510603+xcdh127@users.noreply.github.com>
2021-06-15 15:44:03 +0800

    Add files via upload

---
+++ JianZhi_57.java
@@ -0,0 +1,29 @@
+/**
+ * @program: LeetCodeJavaTest
+ * @description: 剑指offer第57题
+ * @author: Mr.Sun
+ * @create: 2021-06-15 15:41
+ **/
+public class JianZhi_57 {
+}
+
+
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length-1;
+        int[] res = new int[2] ;
+        int sum = 0;
+        while(i<j){
+            sum= nums[i]+nums[j];
+            if(sum<target) i++;
+            if(sum>target) j--;
+            if(sum == target){
+                res[0]=nums[i];
+                res[1]=nums[j];
+                break;
+            }
+        }
+        return res;
+    }
+}
